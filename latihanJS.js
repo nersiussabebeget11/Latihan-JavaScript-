7879b0c4eb3d24787d37e02ac484d3c3d5c8f674
Nersius Sabebeget <nersiussabebeget11@gmail.com>
2023-11-05 21:33:22 +0700

    Latihan JS

---
+++ latihanJS.js
@@ -0,0 +1,84 @@
+//!Incremnet
+let number = 10
+number++
+number++
+number++
+console.log(number)
+
+//! Decrement
+let dataNumber = 11
+dataNumber--
+console.log(dataNumber);
+
+
+// ! Aritmatic /Addition
+let a = 23
+let b = 3
+let c = 3.14
+
+let addOne = a+ b + c
+console.log(addOne);
+console.log(a + b + 10);
+
+// ! Pengurangan / Subtraction
+
+let subOne = a - b
+console.log(subOne);
+
+subOne = b - a
+console.log(subOne);
+
+let x = 30
+console.log(x);
+x = 50
+console.log(x);
+
+// ! Perkalian / Multiplication
+
+console.log(a * b);
+
+
+// ! Modulus ==> Menghitung hasil pembagian
+
+let modTwo = 20 % 2
+
+console.log(modTwo);
+
+let modThree = 20 % 3
+
+console.log(modThree);
+
+let modfour = 10 % 3
+console.log(modfour);
+
+
+// ! bilangan genap
+console.log(10 % 2);
+
+// ! bilangan ganjil
+console.log(9 % 2);
+
+
+// ! Shorthand Operator
+// ? Cara Pertama 1
+
+let score = 2
+score = score + 10
+score = score * 2
+score = score  / 8
+score = score - 3
+console.log(score);
+
+// ? Cara Kedua
+
+let nilaiscore = 2
+nilaiscore += 10
+nilaiscore *= 2
+nilaiscore /= 8
+nilaiscore -= 3
+console.log(nilaiscore);
+
+
+    
+
+
